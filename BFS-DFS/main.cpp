6de04622aa0921c68b634dd9e7a0e2b935040c6f
Baris Sari <sariba@itu.edu.tr>
2020-09-07 21:18:08 +0300

    Reformat code, implement new methods, use best practises

--- BFS-DFS/main.cpp
+++ BFS-DFS/main.cpp
@@ -19,12 +19,15 @@
 #include <stack>
 #include <ctime>
 
-enum variables{Farmer, Rabbit, Carrot, Fox};
+enum variables {
+    Farmer, Rabbit, Carrot, Fox
+};
 
-class Node{
+class Node {
 public:
-    std::string east[4],west[4];
-    int no;
+    std::string east[4], west[4];
+    int no{};
+
     static Node create(int, int, int, variables, variables, variables, variables);
 };
 
@@ -32,73 +35,79 @@ Node Node::create(int e, int w, int number, variables vr, variables vr1, variabl
     Node n;
     std::string str[4];
     variables array[4] = {vr, vr1, vr2, vr3};
-    for (int i=0; i<4; i++) {
-        if(array[i]== Farmer)
+    for (int i = 0; i < 4; i++) {
+        if (array[i] == Farmer)
             str[i] = " Farmer";
-        else if(array[i]==Rabbit)
+        else if (array[i] == Rabbit)
             str[i] = " Rabbit";
-        else if(array[i]==Carrot)
+        else if (array[i] == Carrot)
             str[i] = " Carrot";
         else
             str[i] = " Fox";
     }
-    for(int j=0; j<e; j++)
+    for (int j = 0; j < e; j++)
         n.east[j] = str[j];
-    for(int k=0; k<w; k++)
-        n.west[k] = str[3-k];
+    for (int k = 0; k < w; k++)
+        n.west[k] = str[3 - k];
     n.no = number;
     return n;
 }
 
-class Graph{
+class Graph {
     std::list<class Node> *list;
     int visitedNodes;
     int movCount;
 public:
     Graph(); //constructor
-    void insertion(int, class Node);
-    int* BFS(int s);
-    int* DFS(int s);
-    void print_path(std::list<int>, class Node*);
+    void insert(int, const class Node &);
+
+    int *BFS(int s);
+
+    int *DFS(int s);
+
+    static void print_path(std::list<int>, class Node *);
 
     //helper methods
-    void print_stack(std::stack<int>);
-    void print_queue(std::queue<int>);
+    static void print_stack(std::stack<int>);
+
+    static void print_queue(std::queue<int>);
+
+    void create_example_graph(class Node *nodes);
 };
 
 Graph::Graph() {
-    list = new std::list<class Node> [11];
+    list = new std::list<class Node>[11];
     visitedNodes = 0;
     movCount = 0;
 }
 
-void Graph::insertion(int from, class Node x) {
+void Graph::insert(int from, const class Node &x) {
     list[from].push_back(x);  //add node x to the end of list[from]
 }
 
-int* Graph::BFS(int s){
-    std::list <class Node> :: iterator it;
-    std::list <class Node> temp;
+int *Graph::BFS(int s) {
+    std::list<class Node>::iterator it;
+    std::list<class Node> temp;
     std::queue<int> queue;
     int u;
     int *parent = new int[11];  //its necessary for backtracking
     bool discovered[11];
 
     //initially, parents are -1 and all nodes aren't discovered yet
-    for(int i=0;i<11;i++) {
+    for (int i = 0; i < 11; i++) {
         discovered[i] = false;
         parent[i] = -1;
     }
 
     queue.push(s);
     discovered[s] = true;
-    while(queue.empty() == false){
+    while (!queue.empty()) {
 //        print_queue(queue);
 
         u = queue.front();
         queue.pop();
-        for(it = list[u].begin(); it != list[u].end(); ++it){  //parsing the list[u]
-            if(discovered[(*it).no] == false) {    //if a node hasn't been discovered yet, mark it discovered, set parent and push it to the queue
+        for (it = list[u].begin(); it != list[u].end(); ++it) {  //parsing the list[u]
+            if (!discovered[(*it).no]) {    //if a node hasn't been discovered yet, mark it discovered, set parent and push it to the queue
                 discovered[(*it).no] = true;
                 parent[(*it).no] = u;
                 queue.push((*it).no);
@@ -112,15 +121,15 @@ int* Graph::BFS(int s){
     return parent;
 }
 
-int* Graph::DFS(int s) {
-    std::list <class Node> :: iterator it;
+int *Graph::DFS(int s) {
+    std::list<class Node>::iterator it;
     std::stack<int> stack;
     int u;
     int *parent = new int[11];  //it's necessary for backtracking
     bool discovered[11];
 
     //initially, parents are -1 and all nodes aren't discovered yet
-    for(int i=0;i<11;i++) {
+    for (int i = 0; i < 11; i++) {
         discovered[i] = false;
         parent[i] = -1;
     }
@@ -128,15 +137,15 @@ int* Graph::DFS(int s) {
     stack.push(s);
     discovered[s] = true;
 
-    while (stack.empty() == false){
+    while (!stack.empty()) {
 //        print_stack(stack) ;
 
         u = stack.top();
         stack.pop();
-        if(discovered[u] == false)
-            discovered[u] = true;
-        for(it = list[u].begin(); it != list[u].end(); ++it){  //parsing adjacency list[u]
-            if(discovered[(*it).no] == false) {  //if list[u] has a node that not discovered, push it to stack, set parent of the node
+        if (discovered[u]) {} else discovered[u] = true;
+        for (it = list[u].begin(); it != list[u].end(); ++it) {  //parsing adjacency list[u]
+            if (discovered[(*it).no]) {}
+            else {  //if list[u] has a node that not discovered, push it to stack, set parent of the node
                 stack.push((*it).no);
                 movCount++;
                 parent[(*it).no] = u;
@@ -151,21 +160,22 @@ int* Graph::DFS(int s) {
 }
 
 void Graph::print_path(std::list<int> ls, class Node *nodes) {
-    std::list<int> :: iterator it;
-    std::list<int> :: iterator prev;
+    std::list<int>::iterator it;
+    std::list<int>::iterator prev;
 
-    for(it = ls.begin(), prev = ls.end(); it != ls.end(); prev = it, ++it) { //parsing nodes according to path, prev is the previous node
-        if(prev != ls.end()) {    //if there is previous node
+    for (it = ls.begin(), prev = ls.end();
+         it != ls.end(); prev = it, ++it) { //parsing nodes according to path, prev is the previous node
+        if (prev != ls.end()) {    //if there is previous node
             std::cout << "(Farmer, ";
 
             //if farmer is at east now, that means he comes from west alone or with fox,carrot or rabbit
             if (nodes[*it].east[0] == " Farmer" || nodes[*it].east[1] == " Farmer" || nodes[*it].east[2] == " Farmer" ||
                 nodes[*it].east[3] == " Farmer") {
-                for (int i = 0; i < 4; ++i) {
-                    if (!nodes[*it].east[i].empty() && nodes[*it].east[i] != " Farmer") { //continue if east[i] is not empty or farmer
-                        for (int j = 0; j < 4; ++j) {  //check the thing which was at west before, and it is at east now
-                            if (nodes[*it].east[i] == nodes[*prev].west[j]) { //if it's found, print it and break inner loop
-                                std::cout << nodes[*it].east[i] << ", ";
+                for (auto &i : nodes[*it].east) {
+                    if (!i.empty() && i != " Farmer") { //continue if east[i] is not empty or farmer
+                        for (auto &j : nodes[*prev].west) {  //check the thing which was at west before, and it is at east now
+                            if (i == j) { //if it's found, print it and break inner loop
+                                std::cout << i << ", ";
                                 break;
                             }
                         }
@@ -173,11 +183,11 @@ void Graph::print_path(std::list<int> ls, class Node *nodes) {
                 }
                 std::cout << "<)";
             } else {  //if he is at west now, that means he was east side of the river
-                for (int i = 0; i < 4; ++i) {
-                    if (!nodes[*it].west[i].empty() && nodes[*it].west[i] != " Farmer") { //continue if west[i] is not empty or farmer
-                        for (int j = 0; j < 4; ++j) {  //check the thing which was at east before, and it is at west now
-                            if (nodes[*it].west[i] == nodes[*prev].east[j]) {  //if it's found, print it and break inner loop
-                                std::cout << nodes[*it].west[i] << ", ";
+                for (auto & i : nodes[*it].west) {
+                    if (!i.empty() &&  i != " Farmer") { //continue if west[i] is not empty or farmer
+                        for (auto & j : nodes[*prev].east) {  //check the thing which was at east before, and it is at west now
+                            if (i == j) {  //if it's found, print it and break inner loop
+                                std::cout << i << ", ";
                                 break;
                             }
                         }
@@ -187,32 +197,26 @@ void Graph::print_path(std::list<int> ls, class Node *nodes) {
             }
         }
         std::cout << std::endl << nodes[*it].east[0] << nodes[*it].east[1] << nodes[*it].east[2] << nodes[*it].east[3]
-                  << " ||" << nodes[*it].west[0] << nodes[*it].west[1] << nodes[*it].west[2] << nodes[*it].west[3] << std::endl;
+                  << " ||" << nodes[*it].west[0] << nodes[*it].west[1] << nodes[*it].west[2] << nodes[*it].west[3]
+                  << std::endl;
     }
 }
 
 void Graph::print_stack(std::stack<int> s) {
     while (!s.empty()) {
-        std::cout<<s.top();
+        std::cout << s.top();
         s.pop();
     }
 }
 
 void Graph::print_queue(std::queue<int> q) {
     while (!q.empty()) {
-        std::cout<<q.front();
+        std::cout << q.front();
         q.pop();
     }
 }
 
-int main(int argc, char* argv[]) {
-    std::string method = argv[1];
-    std::list<int> path;
-    int *p;
-    int i;
-    Node *nodes = new Node[11];
-    Graph g;    //creating adjacency list
-
+void Graph::create_example_graph(class Node *nodes) {
     /**creating nodes, first parameter of them are on the east of the river, second parameter
     of them are on the west of the river, third parameter is the node number, and last 4 parameters
      are order of them. For example, 3,1, Farmer, Rabbit,Carrot,Fox means that Farmer,Rabbit and Carrot
@@ -230,47 +234,57 @@ int main(int argc, char* argv[]) {
     nodes[10] = nodes[0].create(0, 4, 10, Farmer, Rabbit, Carrot, Fox);
 
     /**Creating the graph by connecting the nodes*/
-    g.insertion(0, nodes[1]);
-    g.insertion(1, nodes[0]);
+    (*this).insert(0, nodes[1]);
+    (*this).insert(1, nodes[0]);
 
-    g.insertion(0, nodes[2]);
-    g.insertion(2, nodes[0]);
+    (*this).insert(0, nodes[2]);
+    (*this).insert(2, nodes[0]);
 
-    g.insertion(2, nodes[3]);
-    g.insertion(3, nodes[2]);
+    (*this).insert(2, nodes[3]);
+    (*this).insert(3, nodes[2]);
 
-    g.insertion(3, nodes[4]);
-    g.insertion(4, nodes[3]);
+    (*this).insert(3, nodes[4]);
+    (*this).insert(4, nodes[3]);
 
-    g.insertion(3, nodes[5]);
-    g.insertion(5, nodes[3]);
+    (*this).insert(3, nodes[5]);
+    (*this).insert(5, nodes[3]);
 
-    g.insertion(4, nodes[6]);
-    g.insertion(6, nodes[4]);
+    (*this).insert(4, nodes[6]);
+    (*this).insert(6, nodes[4]);
 
-    g.insertion(5, nodes[7]);
-    g.insertion(7, nodes[5]);
+    (*this).insert(5, nodes[7]);
+    (*this).insert(7, nodes[5]);
 
-    g.insertion(6, nodes[8]);
-    g.insertion(8, nodes[6]);
+    (*this).insert(6, nodes[8]);
+    (*this).insert(8, nodes[6]);
 
-    g.insertion(7, nodes[8]);
-    g.insertion(8, nodes[7]);
+    (*this).insert(7, nodes[8]);
+    (*this).insert(8, nodes[7]);
 
-    g.insertion(8, nodes[9]);
-    g.insertion(9, nodes[8]);
+    (*this).insert(8, nodes[9]);
+    (*this).insert(9, nodes[8]);
 
-    g.insertion(9, nodes[10]);
-    g.insertion(10, nodes[9]);
+    (*this).insert(9, nodes[10]);
+    (*this).insert(10, nodes[9]);
+}
+
+int main(int argc, char *argv[]) {
+    std::string method = argv[1];
+    std::list<int> path;
+    int *p = nullptr;
+    int i;
+    Node *nodes = new Node[11];
+    Graph g;    //creating adjacency list
+    g.create_example_graph(nodes);
 
     clock_t begin = clock();
-    if(method == "bfs") {
+    if (method == "bfs") {
         std::cout << "Method : BFS" << std::endl;
         p = g.BFS(0);
-    }else if (method == "dfs"){
+    } else if (method == "dfs") {
         std::cout << "Method : DFS" << std::endl;
-            p = g.DFS(0);
-    }else
+        p = g.DFS(0);
+    } else
         std::cout << "Invalid argument!" << std::endl;
 
     clock_t end = clock();
